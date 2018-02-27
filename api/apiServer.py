4d8b5245e895cfb13b540f3d6b14a041b371b096
xy <monkeysjourney@gmail.com>
2018-02-27 18:39:02 +0800

    逻辑修改

--- api/apiServer.py
+++ api/apiServer.py
@@ -36,7 +36,7 @@ class Used(tornado.web.RequestHandler):
         pass
 
     def post(self):
-        data = json.loads(self.request.body)
+        data = json.loads(str(self.request.body))
 
         proxy_id = data.get('id', None)
         is_succ = data.get('is_succ', None)
