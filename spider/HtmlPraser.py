e6532d9df2608fbce28feceb148745206ace32b7
xy <monkeysjourney@gmail.com>
2018-02-27 11:12:32 +0800

    逻辑修改

--- spider/HtmlPraser.py
+++ spider/HtmlPraser.py
@@ -5,7 +5,6 @@ from util.IPAddress import IPAddresss
 import re
 from util.compatibility import text_
 
-__author__ = 'qiye'
 from lxml import etree
 
 
@@ -14,12 +13,11 @@ class Html_Parser(object):
         self.ips = IPAddresss(QQWRY_PATH)
 
     def parse(self, response, parser):
-        '''
-
+        """
         :param response: 响应
         :param type: 解析方式
         :return:
-        '''
+        """
         if parser['type'] == 'xpath':
             return self.XpathPraser(response, parser)
         elif parser['type'] == 'regular':
@@ -29,142 +27,114 @@ class Html_Parser(object):
         else:
             return None
 
-    def AuthCountry(self, addr):
-        '''
+    @staticmethod
+    def auth_country(addr):
+        """
         用来判断地址是哪个国家的
         :param addr:
         :return:
-        '''
+        """
         for area in CHINA_AREA:
             if text_(area) in addr:
                 return True
         return False
 
+    def parse_ip_to_addr(self, ip):
+        addr = self.ips.getIpAddr(self.ips.str2ip(ip))
+
+        country = ''
+        area = ''
+
+        if '省' in addr or self.auth_country(addr):
+            country = text_('国内')
+            area = addr
+        else:
+            country = text_('国外')
+            area = addr
+
+        return country, area
 
     def XpathPraser(self, response, parser):
-        '''
+        """
         针对xpath方式进行解析
         :param response:
         :param parser:
         :return:
-        '''
-        proxylist = []
+        """
+        proxy_list = []
         root = etree.HTML(response)
-        proxys = root.xpath(parser['pattern'])
-        for proxy in proxys:
+        proxies = root.xpath(parser['pattern'])
+        for proxy in proxies:
             try:
                 ip = proxy.xpath(parser['position']['ip'])[0].text
                 port = proxy.xpath(parser['position']['port'])[0].text
-                type = 0
-                protocol = 0
-                addr = self.ips.getIpAddr(self.ips.str2ip(ip))
-                country = text_('')
-                area = text_('')
-                if text_('省') in addr or self.AuthCountry(addr):
-                    country = text_('国内')
-                    area = addr
-                else:
-                    country = text_('国外')
-                    area = addr
+                country, area = self.parse_ip_to_addr(ip)
             except Exception as e:
                 continue
-            # updatetime = datetime.datetime.now()
-            # ip，端口，类型(0高匿名，1透明)，protocol(0 http,1 https http),country(国家),area(省市),updatetime(更新时间)
 
-            # proxy ={'ip':ip,'port':int(port),'type':int(type),'protocol':int(protocol),'country':country,'area':area,'updatetime':updatetime,'speed':100}
-            proxy = {'ip': ip, 'port': int(port), 'types': int(type), 'protocol': int(protocol), 'country': country,
-                     'area': area, 'speed': 100}
-            proxylist.append(proxy)
-        return proxylist
+            proxy = {'ip': ip, 'port': int(port), 'country': country, 'area': area}
+            proxy_list.append(proxy)
+        return proxy_list
 
     def RegularPraser(self, response, parser):
-        '''
+        """
         针对正则表达式进行解析
         :param response:
         :param parser:
         :return:
-        '''
-        proxylist = []
+        """
+        proxy_list = []
         pattern = re.compile(parser['pattern'])
-        matchs = pattern.findall(response)
-        if matchs != None:
-            for match in matchs:
+        matches = pattern.findall(response)
+        if matches is not None:
+            for match in matches:
                 try:
                     ip = match[parser['position']['ip']]
                     port = match[parser['position']['port']]
-                    # 网站的类型一直不靠谱所以还是默认，之后会检测
-                    type = 0
-                    # if parser['postion']['protocol'] > 0:
-                    # protocol = match[parser['postion']['protocol']]
-                    # if protocol.lower().find('https')!=-1:
-                    #         protocol = 1
-                    #     else:
-                    #         protocol = 0
-                    # else:
-                    protocol = 0
-                    addr = self.ips.getIpAddr(self.ips.str2ip(ip))
-                    country = text_('')
-                    area = text_('')
-                    # print(ip,port)
-                    if text_('省') in addr or self.AuthCountry(addr):
-                        country = text_('国内')
-                        area = addr
-                    else:
-                        country = text_('国外')
-                        area = addr
+                    country, area = self.parse_ip_to_addr(ip)
                 except Exception as e:
                     continue
 
-                proxy = {'ip': ip, 'port': port, 'types': type, 'protocol': protocol, 'country': country, 'area': area,
-                         'speed': 100}
+                proxy = {'ip': ip, 'port': port, 'country': country, 'area': area}
 
-                proxylist.append(proxy)
-            return proxylist
+                proxy_list.append(proxy)
+            return proxy_list
 
 
     def CnproxyPraser(self, response, parser):
-        proxylist = self.RegularPraser(response, parser)
-        chardict = {'v': '3', 'm': '4', 'a': '2', 'l': '9', 'q': '0', 'b': '5', 'i': '7', 'w': '6', 'r': '8', 'c': '1'}
+        """
+        :param response:
+        :param parser:
+        :return:
+        """
+        proxy_list = self.RegularPraser(response, parser)
+        char_dict = {'v': '3', 'm': '4', 'a': '2', 'l': '9', 'q': '0', 'b': '5', 'i': '7', 'w': '6', 'r': '8', 'c': '1'}
 
-        for proxy in proxylist:
+        for proxy in proxy_list:
             port = proxy['port']
             new_port = ''
             for i in range(len(port)):
                 if port[i] != '+':
-                    new_port += chardict[port[i]]
-            new_port = int(new_port)
-            proxy['port'] = new_port
-        return proxylist
-
+                    new_port += char_dict[port[i]]
+            proxy['port'] = int(new_port)
+        return proxy_list
 
     def proxy_listPraser(self, response, parser):
-        proxylist = []
+        proxy_list = []
         pattern = re.compile(parser['pattern'])
-        matchs = pattern.findall(response)
-        if matchs:
-            for match in matchs:
+        matches = pattern.findall(response)
+        if matches:
+            for match in matches:
                 try:
                     ip_port = base64.b64decode(match.replace("Proxy('", "").replace("')", ""))
                     ip = ip_port.split(':')[0]
                     port = ip_port.split(':')[1]
-                    type = 0
-                    protocol = 0
-                    addr = self.ips.getIpAddr(self.ips.str2ip(ip))
-                    country = text_('')
-                    area = text_('')
-                    # print(ip,port)
-                    if text_('省') in addr or self.AuthCountry(addr):
-                        country = text_('国内')
-                        area = addr
-                    else:
-                        country = text_('国外')
-                        area = addr
+                    country, area = self.parse_ip_to_addr(ip)
                 except Exception as e:
                     continue
-                proxy = {'ip': ip, 'port': int(port), 'types': type, 'protocol': protocol, 'country': country,
-                         'area': area, 'speed': 100}
-                proxylist.append(proxy)
-            return proxylist
+                proxy = {'ip': ip, 'port': int(port), 'country': country, 'area': area}
+                proxy_list.append(proxy)
+            return proxy_list
 
 
 
--- spider/ProxyCrawl.py
+++ spider/ProxyCrawl.py
@@ -1,109 +1,101 @@
 # coding:utf-8
-from gevent import monkey
-monkey.patch_all()
-
-import sys
 import time
 import gevent
+from gevent import monkey
 
-from gevent.pool import Pool
-from multiprocessing import Queue, Process, Value
+from util.log import logger, init_log
 
-from api.apiServer import start_api_server
-from config import THREADNUM, parserList, UPDATE_TIME, MINNUM, MAX_CHECK_CONCURRENT_PER_PROCESS, MAX_DOWNLOAD_CONCURRENT
-from db.DataStore import store_data, sqlhelper
+from config import parserList, UPDATE_TIME, MAX_DOWNLOAD_CONCURRENT, MAX_CHECK_CONCURRENT_PER_PROCESS
 from spider.HtmlDownloader import Html_Downloader
 from spider.HtmlPraser import Html_Parser
-from validator.Validator import validator, getMyIP, detect_from_db
+from db.SqlHelper import SqlHelper as SqlHelper
 
-'''
-这个类的作用是描述爬虫的逻辑
-'''
+monkey.patch_all()
 
 
-def startProxyCrawl(queue, db_proxy_num,myip):
-    crawl = ProxyCrawl(queue, db_proxy_num,myip)
+def start_proxy_crawl():
+    crawl = ProxyCrawl()
     crawl.run()
 
 
 class ProxyCrawl(object):
-    proxies = set()
 
-    def __init__(self, queue, db_proxy_num,myip):
-        self.crawl_pool = Pool(THREADNUM)
-        self.queue = queue
-        self.db_proxy_num = db_proxy_num
-        self.myip = myip
+    def __init__(self):
+        self.sqlhelper = SqlHelper()
+        # self.sqlhelper.init_db()
 
+        self.proxies_set = set()
+        self.url_count = 0
+        self.url_total = sum([len(p['urls']) for p in parserList])
 
     def run(self):
         while True:
-            self.proxies.clear()
-            str = 'IPProxyPool----->>>>>>>>beginning'
-            sys.stdout.write(str + "\r\n")
-            sys.stdout.flush()
-            proxylist = sqlhelper.select()
+            self.proxies_set.clear()
+            logger.info('------> loop begin')
 
+            # TODO 删除无效代理
+            logger.info('删除无效代理 %d 个。' % 0)
+
+            # 现有代理列表 现有代理数量
+            count = 0
+            for proxy in self.sqlhelper.select_all():
+                count += 1
+                self.proxies_set.add('%s:%s' % (proxy[0], proxy[1]))
+            begin_num = len(self.proxies_set)
+            logger.info('现有代理数量 %d 个。' % begin_num)
+            if begin_num != count:
+                logger.error('数据库中存在重复代理 -- %d --> %d' % (count, begin_num))
+
+            logger.info('爬取新的代理...')
             spawns = []
-            for proxy in proxylist:
-                spawns.append(gevent.spawn(detect_from_db, self.myip, proxy, self.proxies))
-                if len(spawns) >= MAX_CHECK_CONCURRENT_PER_PROCESS:
+            self.url_count = 0
+            for p in parserList:
+                spawns.append(gevent.spawn(self.crawl, p))
+                if len(spawns) >= MAX_DOWNLOAD_CONCURRENT:
                     gevent.joinall(spawns)
-                    spawns= []
+                    spawns = []
             gevent.joinall(spawns)
-            self.db_proxy_num.value = len(self.proxies)
-            str = 'IPProxyPool----->>>>>>>>db exists ip:%d' % len(self.proxies)
+            spawns.clear()
 
-            if len(self.proxies) < MINNUM:
-                str += '\r\nIPProxyPool----->>>>>>>>now ip num < MINNUM,start crawling...'
-                sys.stdout.write(str + "\r\n")
-                sys.stdout.flush()
-                spawns = []
-                for p in parserList:
-                    spawns.append(gevent.spawn(self.crawl, p))
-                    if len(spawns) >= MAX_DOWNLOAD_CONCURRENT:
-                        gevent.joinall(spawns)
-                        spawns= []
-                gevent.joinall(spawns)
-            else:
-                str += '\r\nIPProxyPool----->>>>>>>>now ip num meet the requirement,wait UPDATE_TIME...'
-                sys.stdout.write(str + "\r\n")
-                sys.stdout.flush()
+            end_num = len(self.proxies_set)
+            logger.info('新代理爬取完成，当前代理数量 %d, 新增加 %d!' % (end_num, end_num - begin_num))
+
+            logger.info('------> loop end, sleep %ds!\n' % UPDATE_TIME)
 
             time.sleep(UPDATE_TIME)
 
     def crawl(self, parser):
+        """
+        爬取
+        :param parser:
+        :return:
+        """
         html_parser = Html_Parser()
         for url in parser['urls']:
             response = Html_Downloader.download(url)
             if response is not None:
-                proxylist = html_parser.parse(response, parser)
-                if proxylist is not None:
-                    for proxy in proxylist:
+                proxy_list = html_parser.parse(response, parser)
+                if proxy_list is not None:
+                    # 检查爬取到的proxy
+                    count, new = 0, 0
+                    for proxy in proxy_list:
+                        count += 1
                         proxy_str = '%s:%s' % (proxy['ip'], proxy['port'])
-                        if proxy_str not in self.proxies:
-                            self.proxies.add(proxy_str)
-                            while True:
-                                if self.queue.full():
-                                    time.sleep(0.1)
-                                else:
-                                    self.queue.put(proxy)
-                                    break
+                        if proxy_str not in self.proxies_set:
+                            self.proxies_set.add(proxy_str)
+                            new += 1
+                            self.sqlhelper.insert(proxy)
+                    self.url_count += 1
+                    logger.info('%d/%d -- <%s> 获取%d, 未记录的%d' %
+                                (self.url_count, self.url_total, url, count, new))
+                else:
+                    self.url_count += 1
+                    logger.warning('%d/%d -- <%s> 解析数据错误' % (self.url_count, self.url_total, url))
+            else:
+                self.url_count += 1
+                logger.warning('%d/%d -- <%s> 下载页面错误' % (self.url_count, self.url_total, url))
 
 
 if __name__ == "__main__":
-    DB_PROXY_NUM = Value('i', 0)
-    q1 = Queue()
-    q2 = Queue()
-    p0 = Process(target=start_api_server)
-    p1 = Process(target=startProxyCrawl, args=(q1, DB_PROXY_NUM))
-    p2 = Process(target=validator, args=(q1, q2))
-    p3 = Process(target=store_data, args=(q2, DB_PROXY_NUM))
-
-    p0.start()
-    p1.start()
-    p2.start()
-    p3.start()
-
-    # spider = ProxyCrawl()
-    # spider.run()
+    init_log()
+    start_proxy_crawl()
