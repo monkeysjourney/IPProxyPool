d9559076ff202343de9c99eb93b54ff7ff49cff3
xy <monkeysjourney@gmail.com>
2018-02-27 18:44:14 +0800

    逻辑修改

--- clientDemo/proxyRequest.py
+++ clientDemo/proxyRequest.py
@@ -1,15 +1,17 @@
-# coding:utf-8
+# -*- coding:UTF-8 -*-
+
+# 官方
+import json
+import time
 import random
 
+# 第三方
+import requests
 
-PROXY_SERVER_ADDR = 'localhost'
-PROXY_SERVER_PORT = 8000
-PROXY_GET_ONCE_NUM = 30
+# own
+from util.log import logger
 
-
-'''
-USER_AGENTS 随机头信息
-'''
+# 请求头 浏览器类型库
 USER_AGENTS = [
     "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; AcooBrowser; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
     "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; Acoo Browser; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.0.04506)",
@@ -48,11 +50,102 @@ USER_AGENTS = [
 ]
 
 
-def get_header():
-    return {
-        'User-Agent': random.choice(USER_AGENTS),
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-        'Accept-Language': 'en-US,en;q=0.5',
-        'Connection': 'keep-alive',
-        'Accept-Encoding': 'gzip, deflate',
-    }
+class ProxyRequest:
+    """
+    代理
+    """
+
+    def __init__(self, addr, port, once_get_num=30, use_flag='default'):
+        """
+        初始化
+        """
+        self.use_flag = use_flag
+        self.proxies = []
+        self.get_url = "http://%s:%d/get?count=%d&use_flag=%s" % \
+                       (addr, port, once_get_num, self.use_flag)
+        self.used_url = "http://%s:%d/used" % (addr, port)
+        self._get_proxy()
+
+    def _get_proxy(self):
+        """
+        获取代理
+        """
+        req = requests.get(self.get_url)
+
+        datas = [('%s:%d' % (p[0], p[1]), p[2]) for p in json.loads(req.text)]
+
+        # 多个线程会同时调用回去接口 导致获取的数据完全相同
+        if not self.proxies:
+            self.proxies += datas
+            # 打印
+            logger.info("Get proxy success, now local num %d." % len(self.proxies))
+
+    def _proxy_used(self, proxy_id, is_succ, speed):
+        """
+        代理使用反馈
+        """
+        req = requests.post(self.used_url, json.dumps({'id': proxy_id, 'is_succ': str(is_succ),
+                                                       'speed': speed, 'use_flag': self.use_flag}
+                                                      )
+                            )
+
+        if not req:
+            logger.error('used post error, %s' % req)
+        elif req.text.find('error') != -1:
+            logger.error('Used post return error.')
+
+    def get(self, url, validator_func, encoding='utf-8', use_proxy=True):
+        """
+        获取页面 并检测内容
+        :param url: url
+        :param validator_func: 内容检测函数
+        :param encoding: 编码
+        :param use_proxy: 是否使用代理
+        :return:
+        """
+        id = url.replace("http://app.jg.eastmoney.com/NewsData/GetNewsText.do?id=", "")\
+            .replace("&cid=3948045045460666", '')\
+            .replace("http://app.jg.eastmoney.com/Notice/GetNoticeText.do?id=", "")\
+            .replace("&cid=3948047684990666", '')
+
+        # 获取页面 一直获取 直到获取到 或者获取了30次
+        for i in range(100):
+            if use_proxy:
+                if not self.proxies:
+                    self._get_proxy()
+            proxy, proxy_id = self.proxies.pop()
+
+            logger.info('%s -- proxy with %s' % (id, proxy))
+
+            try:
+                start = time.time()
+                req = requests.get(url=url,
+                                   headers={'User-Agent': random.choice(USER_AGENTS),
+                                            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+                                            'Accept-Language': 'en-US,en;q=0.5',
+                                            'Connection': 'keep-alive',
+                                            'Accept-Encoding': 'gzip, deflate',
+                                            },
+                                   timeout=(5, 10),  # 超时时间，(连接超时时间，读取超时时间)
+                                   proxies={"http": proxy}
+                                   )
+                req.encoding = encoding
+
+                if req.ok:
+                    speed = round(time.time() - start, 2)
+                    # 解析内容
+                    data = validator_func(req.text)
+                    if data:
+                        # 反馈
+                        self._proxy_used(proxy_id, True, speed)
+                        # 返回
+                        return data
+                else:
+                    # logger.info('request error -- %s' % req)
+                    pass
+            except Exception as e:
+                # logger.error(e)
+                pass
+
+            # logger.info('%s get error, continue, now num is %d' % (id, i))
+            self._proxy_used(proxy_id, False, 0)
